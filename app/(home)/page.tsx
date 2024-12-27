ddd5c5ac2fdfaee103f7bc4f1d1bb6af7ea919ce
Sidney Lima <sidneylimadev@gmail.com>
2024-12-27 16:37:03 -0300

    feat: ensure get dashboard function is protected

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -22,7 +22,7 @@ async function Home({ searchParams: { month } }: HomeProps) {
   }
   const monthIsInvalid = !month || !isMatch(month, "MM");
   if (monthIsInvalid) {
-    redirect("/?month=01");
+    redirect(`/?month=${new Date().getMonth() + 1}`);
   }
 
   const dashboard = await getDashboard(month);
